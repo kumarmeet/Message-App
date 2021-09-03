30f7f93267c387eeed52365580472fbdc3510e76
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-09-03 22:39:53 +0530

    v1.01

--- Message.cpp
+++ Message.cpp
@@ -2,103 +2,114 @@
 
 using namespace std;
 
-class SendMessage
-{
-private:
+#define space(x) {int i; for(i = 0; i < x; i++) cout<<endl;}
+
+class SendMessage {
+  private:
     list<string> _sm;
 
-public:
+  public:
 
-    void set_sm(string s)
-    {
+    void set_sm(string s) {
         this->_sm.push_front(s);
+        auto it = _sm.begin();
+        *it++;
     }
 
-    string get_sm()
-    {
-        return this->_sm.front();
+    string get_sm() {
+        if(!_sm.empty())
+            return this->_sm.front();
+        return "No sent message\n";
     }
 
-    int get_total_msg_sent()
-    {
-      return _sm.size();
+    int get_total_msg_sent() {
+        return _sm.size();
     }
+
+//    list<string>::iterator get_msgs() {
+//        if(!_sm.empty())
+//            return _sm.begin();
+//    }
 };
 
-class ReceiveMessage
-{
-private:
+class ReceiveMessage {
+  private:
     list<string> _rm;
 
-public:
-    void set_rm(string s)
-    {
+  public:
+    void set_rm(string s) {
         this->_rm.push_front(s);
     }
 
-    string get_rm()
-    {
-        return this->_rm.front();
+    string get_rm() {
+        if(!_rm.empty())
+            return this->_rm.front();
+        return "No received message\n";
     }
 
-    int get_total_msg_received()
-    {
-      return _rm.size();
+    int get_total_msg_received() {
+        return _rm.size();
     }
 };
 
-class Message
-{
-private:
+class Message {
+  private:
     SendMessage sm;
     ReceiveMessage rm;
     string userName;
 
-public:
-    Message(string userName)
-    {
-      this->userName = userName;
+  public:
+    Message(string userName) {
+        this->userName = userName;
     }
 
-    void sendMsg(Message &user2, string s)
-    {
+    void sendMsg(Message &user2, string s) {
         sm.set_sm(s);
         user2.rm.set_rm(s);
     }
 
-    void showSendMsg()
-    {
-        cout<<userName<<" -> "<<this->sm.get_sm()<<endl;
+    void showSendMsg() {
+        cout<<userName<<" sent -> "<<this->sm.get_sm()<<endl;
     }
 
-    void showReceiveMsg()
-    {
-        cout<<userName<<" -> "<<this->rm.get_rm()<<endl;
+    void showReceiveMsg() {
+        cout<<userName<<" received -> "<<this->rm.get_rm()<<endl;
     }
 
-    void totalSentMsg()
-    {
-      cout<<userName<<" -> "<<sm.get_total_msg_sent()<<endl;
+    void totalSentMsgCount() {
+        cout<<userName<<" has total sent messages are -> "<<sm.get_total_msg_sent()<<endl;
     }
 
-    void totalReceivedMsg()
-    {
-      cout<<userName<<" -> "<<rm.get_total_msg_received()<<endl;
+    void totalReceivedMsgCount() {
+        cout<<userName<<" total received messages are -> "<<rm.get_total_msg_received()<<endl;
     }
+
+//    void findMsg(string s) {
+//        auto it = sm.get_msgs();
+//        cout<<*it--;
+//        return;
+//    }
+
 };
 
-int main()
-{
+int main() {
     Message user1("Tony"), user2("Rohan");
 
     user1.sendMsg(user2, "Hello!");
-    user1.sendMsg(user2, "How are you?");
-    user1.showSendMsg();
     user2.showReceiveMsg();
-
     user2.sendMsg(user1, "Hey!");
-    user2.sendMsg(user1, "I am fine, what about you?");
-    user2.showSendMsg();
     user1.showReceiveMsg();
+    user1.sendMsg(user2, "How are you?");
+    user2.showReceiveMsg();
+    user2.sendMsg(user1, "I am fine, what about you?");
+    user1.showReceiveMsg();
+
+    space(2);
+    user1.totalReceivedMsgCount();
+    user2.totalReceivedMsgCount();
+
+    space(2);
+//    user1.findMsg("Hello");
+
     return 0;
 }
